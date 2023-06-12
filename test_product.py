af6dacdef950be2eb28370603476bd672e3b2f61
foxa05 <136233291+foxa05@users.noreply.github.com>
2023-06-12 19:45:56 +0200

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -1,20 +1,24 @@
 import pytest
 from products import Product
 
+
 def test_create_normal_product():
     product = Product("MacBook Air M2", price=1450, quantity=100)
     assert product.get_quantity() == 100
     assert product.is_active() is True
 
+
 def test_create_invalid_product():
-    with pytest.raises(Exception):
+    with pytest.raises(ValueError):
         product = Product("", price=-500, quantity=50)  # Empty name and negative price
+
 
 def test_product_becomes_inactive():
     product = Product("Headphones", price=50, quantity=1)
     assert product.is_active() is True
     product.buy(1)
     assert product.is_active() is False
+
 
 def test_product_purchase():
     product = Product("Phone", price=300, quantity=10)
@@ -23,8 +27,8 @@ def test_product_purchase():
     assert product.get_quantity() == 7
     assert total_price == 900
 
+
 def test_buying_larger_quantity():
     product = Product("Tablet", price=200, quantity=5)
     with pytest.raises(Exception):
         total_price = product.buy(10)
-
