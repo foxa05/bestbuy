49a04a9dbb529d3c621f9706fd0d7a2340bbe610
foxa05 <136233291+foxa05@users.noreply.github.com>
2023-06-12 19:46:54 +0200

    Update products.py

--- products.py
+++ products.py
@@ -1,23 +1,23 @@
+from abc import ABC, abstractmethod
+
+
 class Product:
     def __init__(self, name, price, quantity):
-        if not name:
-            raise ValueError("Name cannot be empty")
-        if price < 0:
-            raise ValueError("Price cannot be negative")
-        if quantity < 0:
-            raise ValueError("Quantity cannot be negative")
-
         self.name = name
         self.price = price
         self.quantity = quantity
         self.active = True
+        self.promotion = None
+
+        if not name or price < 0 or quantity < 0:
+            raise ValueError("Invalid product details")
 
     def get_quantity(self):
         return self.quantity
 
     def set_quantity(self, quantity):
         self.quantity = quantity
-        if self.quantity == 0:
+        if quantity == 0:
             self.deactivate()
 
     def is_active(self):
@@ -29,37 +29,93 @@ class Product:
     def deactivate(self):
         self.active = False
 
+    def get_promotion(self):
+        return self.promotion
+
+    def set_promotion(self, promotion):
+        self.promotion = promotion
+
     def show(self):
-        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+        promotion_info = self.promotion.name if self.promotion else "None"
+        return f"{self.name}, Price: ${self.price}, Quantity: {self.quantity}, Promotion: {promotion_info}"
 
     def buy(self, quantity):
-        if not self.active:
-            raise Exception("Product is not active")
-        if quantity > self.quantity:
-            raise Exception("Insufficient quantity available")
-
-        total_price = self.price * quantity
-        self.quantity -= quantity
-        if self.quantity == 0:
-            self.deactivate()
-
-        return total_price
-
-
-def main():
-    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-    mac = Product("MacBook Air M2", price=1450, quantity=100)
-
-    print(bose.buy(50))
-    print(mac.buy(100))
-    print(mac.is_active())
-
-    bose.show()
-    mac.show()
-
-    bose.set_quantity(1000)
-    bose.show()
+        if quantity <= self.quantity:
+            self.quantity -= quantity
+            if self.quantity == 0:
+                self.deactivate()
+            if self.promotion:
+                return self.promotion.apply_promotion(self) * quantity
+            else:
+                return self.price * quantity
+        else:
+            raise Exception("Insufficient quantity available for purchase")
 
 
-if __name__ == '__main__':
-    main()
+
+class LimitedProduct(Product):
+    def __init__(self, name, price, quantity, maximum):
+        super().__init__(name, price, quantity)
+        self.maximum = maximum
+
+    def show(self):
+        promotion_info = self.promotion.name if self.promotion else "None"
+        quantity_info = f"Quantity: {self.quantity}, Limited to {self.maximum} per order!"
+        return f"{self.name}, Price: ${self.price}, {quantity_info}, Promotion: {promotion_info}"
+
+    def buy(self, quantity):
+        if quantity <= self.quantity and quantity <= self.maximum:
+            self.quantity -= quantity
+            if self.promotion:
+                return self.promotion.apply_promotion(self) * quantity
+            else:
+                return self.price * quantity
+        else:
+            raise Exception("Invalid quantity or exceeds maximum limit")
+
+
+class Promotion(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def apply_promotion(self, product):
+        pass
+
+
+class SecondHalfPrice(Promotion):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def apply_promotion(self, product):
+        return product.price / 2
+
+
+class ThirdOneFree(Promotion):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def apply_promotion(self, product):
+        quantity_to_pay = product.quantity - (product.quantity // 3)
+        return product.price * quantity_to_pay
+
+
+class PercentDiscount(Promotion):
+    def __init__(self, name, percent):
+        super().__init__(name)
+        self.percent = percent
+
+    def apply_promotion(self, product):
+        discount = (self.percent / 100) * product.price
+        return product.price - discount
+
+
+class NonStockedProduct(Product):
+    def __init__(self, name, price):
+        super().__init__(name, price, 0)
+
+    def set_quantity(self, quantity):
+        raise Exception("Cannot set quantity for a non-stocked product")
+
+    def buy(self, quantity):
+        raise Exception("Cannot purchase a non-stocked product")
